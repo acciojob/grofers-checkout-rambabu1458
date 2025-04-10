285c335a6847826707e4806cae1039d865b60ada
rambabu1458 <58924997+rambabu1458@users.noreply.github.com>
2025-04-10 21:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   const table=document.querySelector("table");
+
+        let priceElements=table.querySelectorAll(".prices");
+
+        let total=0;
+        priceElements.forEach(price=>{
+            const value=parseFloat(price.textContent);
+            if(!isNaN(value)){
+                total+=value;
+            }
+        })
+        const totalRow=document.createElement("tr");
+
+        const totalCell=document.createElement("td");
+        totalCell.colSpan=2;
+        totalCell.textContent=`Total Price : $${total.toFixed(2)}`;
+
+        totalRow.appendChild(totalCell);
+
+        table.appendChild(totalRow)
+
 };
 
 getSumBtn.addEventListener("click", getSum);
